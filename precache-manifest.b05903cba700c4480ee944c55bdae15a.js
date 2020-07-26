cdd9a0831acde2e62415f79c81da7bcbd0258936
Ram3093 <65605721+Ram3093@users.noreply.github.com>
2020-07-27 00:48:40 +0530

    Updates

---
+++ precache-manifest.b05903cba700c4480ee944c55bdae15a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6e3c8c6b32248e2d487f86bd90f40c",
+    "url": "/salozone-service/index.html"
+  },
+  {
+    "revision": "4b335eb2f8abeb49b866",
+    "url": "/salozone-service/static/css/2.94fff46c.chunk.css"
+  },
+  {
+    "revision": "885429ba6017f1896736",
+    "url": "/salozone-service/static/css/main.bc8473c6.chunk.css"
+  },
+  {
+    "revision": "4b335eb2f8abeb49b866",
+    "url": "/salozone-service/static/js/2.27a56d09.chunk.js"
+  },
+  {
+    "revision": "c1eee29b3c8c73deb636c24b71aef64c",
+    "url": "/salozone-service/static/js/2.27a56d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885429ba6017f1896736",
+    "url": "/salozone-service/static/js/main.ee989e17.chunk.js"
+  },
+  {
+    "revision": "daf3fa721578f5b3cb64",
+    "url": "/salozone-service/static/js/runtime-main.d1ecfb32.js"
+  },
+  {
+    "revision": "ce43a36f274cf79d9b793e8acf110b9f",
+    "url": "/salozone-service/static/media/img1.ce43a36f.jpg"
+  },
+  {
+    "revision": "e42948a3ebf8c3082787756892e00262",
+    "url": "/salozone-service/static/media/img2.e42948a3.jpg"
+  },
+  {
+    "revision": "743f9c215615e0c4c940aca081fed94f",
+    "url": "/salozone-service/static/media/img3.743f9c21.jpg"
+  }
+]);
